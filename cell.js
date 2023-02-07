bbef85836526e9e7cdbfa71b3ae85c6f9382f645
xwojdat <123936050+xwojdat@users.noreply.github.com>
2023-02-07 09:47:59 +0000

    at some point working

--- cell.js
+++ cell.js
@@ -8,7 +8,7 @@ function cell(i, j, w){
     this.w = w;
     this.neighborCount = 0;
     this.mine = false;
-    this.revealed = true;
+    this.revealed = false;
 }
 
 cell.prototype.show = function(){
@@ -36,7 +36,8 @@ cell.prototype.show = function(){
 
 cell.prototype.countMines = function(){
     if(this.mine){
-        return -1;
+        this.neighborCount = -1;
+        return;
     }
 
     var total = 0;
@@ -60,6 +61,24 @@ cell.prototype.contains = function(x, y){
     return (x > this.x && x< this.x + this.w && y > this.y && y < this.y + this.w);
 }
 
-cell.prototype.reveal = function(x, y){
+cell.prototype.reveal = function(){
     this.revealed = true;
+    if(this.neighborCount == 0){
+        this.floodFill();
+    }
 }
+
+cell.prototype.floodFill = function(){
+    for (var xoff = -1; xoff <= 1; xoff++){
+        for (var yoff = -1; yoff <= 1; yoff++){
+            var i = this.i + xoff;
+            var j = this.j + yoff;
+            if(i > -1 && i < cols && j > -1 && j < rows){
+                var neighbor = grid[i] [j];
+                if(!neighbor.mine && !neighbor.revealed){
+                    neighbor.reveal();
+                }
+                }
+            }
+        }
+    }
